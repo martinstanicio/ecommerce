bf8b7dccb3862ecc00cd39bdcf80748f15c8abcf
Martín Stanicio <martuytimo@gmail.com>
2025-03-05 19:12:21 -0300

    add missing access control utils

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,9 +1,10 @@
+import { anyone } from '@/lib/access'
 import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
-    read: () => true,
+    read: anyone,
   },
   fields: [
     {
--- src/lib/access.ts
+++ src/lib/access.ts
@@ -2,10 +2,16 @@ import type { Access } from 'payload'
 
 export const anyone: Access = () => true
 
-export const admin: Access = ({ req: { user } }) => {
+export const currentUser: Access = ({ req: { user }, id }) => {
   if (!user) return false
 
-  return user.role === 'admin'
+  return user.id === id
+}
+
+export const editorOrCurrentUser: Access = ({ req: { user }, id }) => {
+  if (!user) return false
+
+  return user.role === 'editor' || user.id === id
 }
 
 export const editor: Access = ({ req: { user } }) => {
@@ -14,14 +20,20 @@ export const editor: Access = ({ req: { user } }) => {
   return user.role === 'editor'
 }
 
-export const currentUser: Access = ({ req: { user }, id }) => {
+export const editorOrAdmin: Access = ({ req: { user } }) => {
   if (!user) return false
 
-  return user.id === id
+  return user.role === 'editor' || user.role === 'admin'
 }
 
 export const adminOrCurrentUser: Access = ({ req: { user }, id }) => {
   if (!user) return false
 
   return user.role === 'admin' || user.id === id
+}
+
+export const admin: Access = ({ req: { user } }) => {
+  if (!user) return false
+
+  return user.role === 'admin'
 }
