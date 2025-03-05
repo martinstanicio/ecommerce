b94b9cadcf30e7705e6293102cf56d0b52efd1b2
Martín Stanicio <martuytimo@gmail.com>
2025-03-05 14:24:53 -0300

    sort imports with prettier

--- src/components/brand.tsx
+++ src/components/brand.tsx
@@ -1,6 +1,7 @@
 'use client'
-import { siteName } from '@/lib/metadata'
+
 import Icon from './icon'
+import { siteName } from '@/lib/metadata'
 
 export default function Brand({ style, ...props }: React.HTMLProps<HTMLDivElement>) {
   return (
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,19 +1,16 @@
 // storage-adapter-import-placeholder
+import { Media } from './collections/Media'
+import { Users } from './collections/Users'
+import { siteName } from '@/lib/metadata'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
-
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
-
 import { en } from '@payloadcms/translations/languages/en'
 import { es } from '@payloadcms/translations/languages/es'
-
-import { siteName } from '@/lib/metadata'
+import path from 'path'
+import { buildConfig } from 'payload'
+import sharp from 'sharp'
+import { fileURLToPath } from 'url'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
