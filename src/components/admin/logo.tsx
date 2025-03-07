7c7eb2a0a3a1d363490ccd27dbd7677bdc2b9dc7
Martín Stanicio <martuytimo@gmail.com>
2025-03-07 19:21:03 -0300

    remove brand component

--- src/components/admin/logo.tsx
+++ src/components/admin/logo.tsx
@@ -1,5 +1,11 @@
-import Brand from "@/components/brand";
+import Icon from "@/components/icon";
+import { siteName } from "@/lib/metadata";
 
 export default function Logo() {
-  return <Brand />;
+  return (
+    <div className="flex justify-center items-center gap-4 flex-wrap">
+      <Icon className="h-16" />
+      <span className="text-5xl font-bold">{siteName}</span>
+    </div>
+  );
 }
