cce6a78d191f3f32dcab54c3a2b76165d9951290
Martín Stanicio <martuytimo@gmail.com>
2025-04-11 01:01:28 -0300

    add dynamic metadata to productos page

--- src/app/(frontend)/productos/page.tsx
+++ src/app/(frontend)/productos/page.tsx
@@ -4,6 +4,7 @@ import ProductsGrid from "@/components/products-grid";
 import SearchBar from "@/components/search-bar";
 import SortingBar from "@/components/sorting-bar";
 import config from "@/payload.config";
+import { Metadata } from "next";
 import { getPayload } from "payload";
 
 type Props = {
@@ -51,4 +52,34 @@ export default async function Productos({ searchParams }: Props) {
       />
     </div>
   );
+}
+
+export async function generateMetadata({
+  searchParams,
+}: Props): Promise<Metadata> {
+  const { search } = await searchParams;
+
+  let title = "Productos";
+  let description =
+    "Descubrí nuestra amplia gama de productos de alta calidad para cada necesidad.";
+  const url = "/productos";
+
+  if (search) {
+    const payload = await getPayload({ config });
+    const { totalDocs } = await payload.count({
+      collection: "products",
+      where: {
+        or: [{ name: { like: search } }, { description: { like: search } }],
+      },
+    });
+
+    title = `"${search}"`;
+    description = `${totalDocs === 1 ? "1 resultado" : `${totalDocs} resultados`} de búsqueda para el término "${search}".`;
+  }
+
+  return {
+    title,
+    description,
+    openGraph: { title, description, url },
+  };
 }
