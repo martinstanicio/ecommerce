e1ec7e8b4e9ffe9db74ed36c1c58624853290350
Martín Stanicio <martuytimo@gmail.com>
2025-04-13 17:19:36 -0300

    make carousel rounded

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -137,7 +137,7 @@ function CarouselContent({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       ref={carouselRef}
-      className="overflow-hidden"
+      className="overflow-hidden rounded-md"
       data-slot="carousel-content"
     >
       <div
