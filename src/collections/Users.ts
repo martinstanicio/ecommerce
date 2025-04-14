bbe3a0d2871713aa59375b09d51336af3189f136
Martín Stanicio <martuytimo@gmail.com>
2025-04-14 00:49:18 -0300

    create hook to ensure there is always at least one admin in the system

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,5 +1,7 @@
 import { adminOrCurrentUser, admin } from "@/lib/access";
-import type { CollectionConfig } from "payload";
+import config from "@/payload.config";
+import { getPayload } from "payload";
+import type { CollectionConfig, Where } from "payload";
 
 export const Users: CollectionConfig = {
   slug: "users",
@@ -31,6 +33,30 @@ export const Users: CollectionConfig = {
       required: true,
       access: {
         update: ({ req: { user } }) => user?.role === "admin",
+      },
+      hooks: {
+        beforeChange: [
+          async ({ value, operation, originalDoc }) => {
+            // If the user being created or updated has already been assigned
+            // the admin role, there's no need to check for other admins
+            if (value === "admin") return value;
+
+            let where: Where = { role: { equals: "admin" } };
+            if (operation === "update" && originalDoc && originalDoc.id) {
+              where = { ...where, id: { not_equals: originalDoc.id } };
+            }
+
+            // UPDATE: If the current user is an admin, it will be excluded from the count
+            const payload = await getPayload({ config });
+            const admins = await payload.count({ collection: "users", where });
+
+            // CREATE: If there are no admins in the system, set the role to admin
+            // UPDATE: If there are no other admins in the system, keep the role as admin
+            if (admins.totalDocs === 0) return "admin";
+
+            return value;
+          },
+        ],
       },
     },
   ],
