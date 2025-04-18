a5e0a7cfb0bd8e48f5bed1639c2ccb00cf2f3732
Martín Stanicio <martuytimo@gmail.com>
2025-04-18 20:06:28 -0300

    create product card and grid skeletons

---
+++ src/components/product-card-skeleton.tsx
@@ -0,0 +1,36 @@
+import { Badge } from "./ui/badge";
+import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
+import { Skeleton } from "./ui/skeleton";
+import { cn } from "@/lib/utils";
+
+export default function ProductCardSkeleton({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const tags = new Array(2).fill(null);
+
+  return (
+    <Card className={cn("gap-0 overflow-hidden py-0", className)} {...props}>
+      <Skeleton className="aspect-[4/3]" />
+      <CardContent className="flex h-full flex-col justify-between p-4">
+        <div className="space-y-4">
+          <CardTitle className="flex items-start justify-between">
+            <Skeleton className="h-6 w-[16ch]" />
+            <Skeleton className="h-6 w-[8ch]" />
+          </CardTitle>
+          <CardDescription className="space-y-1">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-2/3" />
+          </CardDescription>
+        </div>
+        <div className="after:to-background relative mt-4 flex gap-1 overflow-x-hidden after:absolute after:right-0 after:h-full after:w-4 after:bg-gradient-to-r after:from-transparent">
+          {tags.map((_, i) => (
+            <Badge key={i} variant="secondary">
+              <Skeleton className="h-2 w-[6ch]"></Skeleton>
+            </Badge>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/components/products-grid-skeleton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import ProductCardSkeleton from "./product-card-skeleton";
+import { cn } from "@/lib/utils";
+
+export default function ProductsGridSkeleton({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const products = new Array(4).fill(null);
+
+  return (
+    <main
+      className={cn(
+        "grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4",
+        className,
+      )}
+      {...props}
+    >
+      {products.map((_, i) => (
+        <ProductCardSkeleton key={i} />
+      ))}
+    </main>
+  );
+}
