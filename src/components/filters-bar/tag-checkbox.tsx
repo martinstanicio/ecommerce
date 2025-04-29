70a38c0997a6fbd779ecd9a7a3a45c7e8a9ac590
Martín Stanicio <martuytimo@gmail.com>
2025-04-29 19:02:39 -0300

    allow filters to be added and removed using checkboxes

--- src/components/filters-bar/tag-checkbox.tsx
+++ src/components/filters-bar/tag-checkbox.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Checkbox } from "../ui/checkbox";
 import { Label } from "../ui/label";
 import { SimplifiedTag } from "./common";
 import { cn } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 type Props = React.ComponentProps<"div"> & {
   tag: SimplifiedTag;
@@ -9,10 +13,37 @@ type Props = React.ComponentProps<"div"> & {
 
 export default function TagCheckbox({ tag, className, ...props }: Props) {
   const { id, name } = tag;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [isChecked, setIsChecked] = useState(
+    searchParams.getAll("tags").includes(name),
+  );
+
+  const handleCheckedChange = (checked: boolean) => {
+    const newParams = new URLSearchParams(searchParams);
+
+    const currentTags = searchParams.getAll("tags");
+    const newTags = checked
+      ? [...currentTags, name]
+      : currentTags.filter((tag) => tag !== name);
+
+    newParams.delete("tags");
+    newTags.forEach((tag) => newParams.append("tags", tag));
+
+    router.replace(`?${newParams.toString()}`);
+    setIsChecked(checked);
+  };
 
   return (
     <div className={cn("flex items-center", className)} {...props}>
-      <Checkbox id={id} className="size-5" />
+      <Checkbox
+        id={id}
+        className="size-5"
+        checked={isChecked}
+        onCheckedChange={(checked) =>
+          handleCheckedChange(typeof checked === "boolean" ? checked : false)
+        }
+      />
       <Label htmlFor={id} className="text-md pl-3">
         {name}
       </Label>
